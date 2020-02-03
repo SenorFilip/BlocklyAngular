e98e424e2d262acebc64837910fd6596a2085ff1
SenorFilip <filipcicfilip1@gmail.com>
2020-02-03 22:34:13 +0100

    Added Blockly

--- src/app/blockly/blockly.component.ts
+++ src/app/blockly/blockly.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { toolbox } from './toolbox';
+
+declare var Blockly: any;
 
 @Component({
   selector: 'app-blockly',
@@ -7,9 +10,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BlocklyComponent implements OnInit {
 
+  code: string;
+  workspace: any;
+  // blocklyDiv: HTMLElement;
+
   constructor() { }
 
   ngOnInit() {
+    // this.blocklyDiv = document.getElementById('blocklyDiv');
+      this.workspace = Blockly.inject('blocklyDiv', toolbox);
+      // change listener on workspace update event
+      this.workspace.addChangeListener(() => {
+        this.code = Blockly.Python.workspaceToCode(this.workspace);
+      });
   }
 
 }
---
+++ src/app/blockly/toolbox.ts
@@ -0,0 +1,88 @@
+export const toolbox = {
+    toolbox: `
+    <xml id="toolbox" style="display: none">
+      <category name="Control" colour="120">
+        <block type="controls_if" disabled="true"></block>
+        <block type="controls_whileUntil"></block>
+        <block type="controls_repeat_ext" disabled="true"></block>
+      </category>
+      <category name="Text" colour="230">
+        <block type="text"></block>
+        <block type="text_print"></block>
+      </category>
+      <category name="Core">
+        <category name="Control">
+          <block type="controls_if"></block>
+          <block type="controls_whileUntil"></block>
+        </category>
+        <category name="Logic">
+          <block type="logic_compare"></block>
+          <block type="logic_operation"></block>
+          <block type="logic_boolean"></block>
+        </category>
+      </category>
+      <sep></sep>
+      <category name="Custom">
+        <block type="logic_boolean"></block>
+        <block type="math_number">
+          <field name="NUM">42</field>
+        </block>
+        <block type="controls_for">
+          <value name="FROM">
+            <block type="math_number">
+              <field name="NUM">1</field>
+            </block>
+          </value>
+          <value name="TO">
+            <block type="math_number">
+              <field name="NUM">10</field>
+            </block>
+          </value>
+          <value name="BY">
+            <block type="math_number">
+              <field name="NUM">1</field>
+            </block>
+          </value>
+        </block>
+        <block type="math_arithmetic">
+          <field name="OP">ADD</field>
+          <value name="A">
+            <shadow type="math_number">
+              <field name="NUM">1</field>
+            </shadow>
+          </value>
+          <value name="B">
+            <shadow type="math_number">
+              <field name="NUM">1</field>
+            </shadow>
+          </value>
+        </block>
+      </category>
+      <category name="Test" colour="44">
+        <block type="logic_operation"></block>
+        <label text="A label" web-class="myLabelStyle"></label>
+        <label text="Another label"></label>
+        <block type="logic_negate"></block>
+        <button text="A button" callbackKey="myFirstButtonPressed"></button>
+        <block type="logic_boolean"></block>
+      </category>
+    </xml>`,
+    grid: {
+        spacing: 20,
+        length: 3,
+        colour: '#167729',
+        snap: true},
+    zoom: {
+        controls: true,
+        wheel: false,
+        startScale: 1.0,
+        maxScale: 3,
+        minScale: 0.3,
+        scaleSpeed: 1.2},
+    move: {
+        scrollbars: true,
+        drag: true,
+        wheel: false},
+    trashcan: true,
+    scrollbars: false
+};
