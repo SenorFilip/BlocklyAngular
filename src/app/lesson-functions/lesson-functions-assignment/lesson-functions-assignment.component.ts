35f7407fdcdfda73969f37b38f92156676aff801
SenorFilip <filipcicfilip1@gmail.com>
2021-01-08 02:16:49 +0100

    Finished the FUNCTIONS task

--- src/app/lesson-functions/lesson-functions-assignment/lesson-functions-assignment.component.ts
+++ src/app/lesson-functions/lesson-functions-assignment/lesson-functions-assignment.component.ts
@@ -8,9 +8,10 @@ import {
   MyMovesFunctionBlock,
   MyMovesFunctionBodyBlock
 } from '../../shared/custom.blocks';
-import {Application, Loader, Sprite, Spritesheet, utils} from 'pixi.js';
+import {AnimatedSprite, Application, Loader, Sprite, Spritesheet, Texture, utils} from 'pixi.js';
 import {AlertService} from '../../shared/alert';
 import * as Blockly from 'ngx-blockly/scripts/blockly/typings/blockly';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-lesson-functions-assignment',
@@ -20,7 +21,18 @@ import * as Blockly from 'ngx-blockly/scripts/blockly/typings/blockly';
 export class LessonFunctionsAssignmentComponent implements OnInit, OnDestroy {
 
   buttonsDisabled = false;
-  fieldArray = [
+  isMouseRunning = false;
+  currentMove;
+  currentBlock = 0;
+  currentGridPosition = {
+    row: 1,
+    column: 0
+  };
+  lastCurve = {
+    row: undefined,
+    column: undefined
+  };
+  gridArray = [
     [0, 0, 0, 0, 2, 1, 1, 2, 1, 5],
     [1, 1, 2, 0, 1, 0, 0, 1, 0, 0],
     [0, 0, 1, 0, 1, 0, 0, 1, 0, 0],
@@ -41,6 +53,21 @@ export class LessonFunctionsAssignmentComponent implements OnInit, OnDestroy {
   rendererWidth: number;
   rendererHeight: number;
   sheet: Spritesheet;
+  mouseAnimatedSprite: AnimatedSprite;
+  mouseSpriteDead: Sprite;
+  cat1Sprite: AnimatedSprite;
+  cat2Sprite: AnimatedSprite;
+  trapSprite: Sprite;
+  cheeseSprite: Sprite;
+  paddingTopBottom: number;
+  paddingLeftRight: number;
+  cellHeight: number;
+  cellWidth: number;
+  texturesRight: Texture[] = [];
+  texturesLeft: Texture[] = [];
+  texturesUp: Texture[] = [];
+  texturesDown: Texture[] = [];
+  textureMouseWon: Texture[] = [];
 
   // Blockly variables
   @ViewChild(NgxBlocklyComponent) workspace;
@@ -72,6 +99,7 @@ export class LessonFunctionsAssignmentComponent implements OnInit, OnDestroy {
   ];
 
   constructor(private ngxToolboxBuilderService: NgxToolboxBuilderService,
+              private router: Router,
               public alertService: AlertService) {
     this.ngxToolboxBuilderService.nodes = this.customBlocks;
     this.config.toolbox = this.ngxToolboxBuilderService.build();
@@ -81,6 +109,10 @@ export class LessonFunctionsAssignmentComponent implements OnInit, OnDestroy {
     this.canvas = document.getElementById('pixiJsFunctionsCanvas');
     this.rendererWidth = this.canvas.offsetWidth;
     this.rendererHeight = this.canvas.offsetHeight;
+    this.paddingTopBottom = this.rendererHeight / 14;
+    this.paddingLeftRight = this.rendererWidth / 22;
+    this.cellHeight = this.paddingTopBottom;
+    this.cellWidth = this.paddingLeftRight * 2;
 
     this.pixiApp = new Application({
       view: this.canvas,
@@ -94,6 +126,11 @@ export class LessonFunctionsAssignmentComponent implements OnInit, OnDestroy {
     Loader.shared.load(() => {
       this.setup();
     });
+
+    this.pixiApp.ticker.add(() => {
+      this.gameLoop();
+    });
+    this.pixiApp.ticker.maxFPS = 30;
   }
 
   setup() {
@@ -106,6 +143,165 @@ export class LessonFunctionsAssignmentComponent implements OnInit, OnDestroy {
     background.height = this.rendererHeight;
 
     this.pixiApp.stage.addChild(background);
+
+    // create animated sprite for the cats
+    this.cat1Sprite = new AnimatedSprite(this.sheet.animations.cat);
+    this.cat1Sprite.animationSpeed = 0.055;
+    this.cat1Sprite.width = 90;
+    this.cat1Sprite.height = 90;
+    this.cat1Sprite.anchor.set(0.5);
+    this.cat1Sprite.position.set(this.paddingLeftRight + 9.5 * this.cellWidth, this.paddingTopBottom + 0.5 * this.cellHeight);
+    this.cat1Sprite.play();
+    this.pixiApp.stage.addChild(this.cat1Sprite);
+
+    this.cat2Sprite = new AnimatedSprite(this.sheet.animations.cat);
+    this.cat2Sprite.animationSpeed = 0.055;
+    this.cat2Sprite.width = 90;
+    this.cat2Sprite.height = 90;
+    this.cat2Sprite.anchor.set(0.5);
+    this.cat2Sprite.position.set(this.paddingLeftRight + 2.5 * this.cellWidth, this.paddingTopBottom + 11.5 * this.cellHeight);
+    this.cat2Sprite.play();
+    this.pixiApp.stage.addChild(this.cat2Sprite);
+
+    // create sprite for mouse trap
+    this.trapSprite = new Sprite(this.sheet.textures['mouseTrap.png']);
+    this.trapSprite.height = 45;
+    this.trapSprite.width = 70;
+    this.trapSprite.anchor.set(0.5);
+    this.trapSprite.position.set(this.paddingLeftRight + 0.5 * this.cellWidth, this.paddingTopBottom + this.cellHeight * 6.5);
+    this.pixiApp.stage.addChild((this.trapSprite));
+
+    // create sprite for cheese
+    this.cheeseSprite = new Sprite(this.sheet.textures['cheese.png']);
+    this.cheeseSprite.height = 50;
+    this.cheeseSprite.width = 60;
+    this.cheeseSprite.anchor.set(0.5);
+    this.cheeseSprite.position.set(this.paddingLeftRight + 9.5 * this.cellWidth, this.paddingTopBottom + this.cellHeight * 10.5);
+    this.pixiApp.stage.addChild((this.cheeseSprite));
+
+    // sprite for the dead mouse
+    this.mouseSpriteDead = new Sprite(this.sheet.textures['mouseDead.png']);
+    this.mouseSpriteDead.height = 60;
+    this.mouseSpriteDead.width = 60;
+    this.mouseSpriteDead.anchor.set(0.5);
+    this.mouseSpriteDead.visible = false;
+    this.pixiApp.stage.addChild((this.mouseSpriteDead));
+
+    // mouse animation sprite
+    this.mouseAnimatedSprite = new AnimatedSprite(this.sheet.animations.mouseRight);
+    this.mouseAnimatedSprite.animationSpeed = 0.055;
+    this.mouseAnimatedSprite.width = 60;
+    this.mouseAnimatedSprite.height = 40;
+    this.mouseAnimatedSprite.anchor.set(0.5);
+    this.mouseAnimatedSprite.position.set(this.paddingLeftRight + 0.5 * this.cellWidth, this.paddingTopBottom + this.cellHeight * 1.5);
+    this.mouseAnimatedSprite.stop();
+    this.pixiApp.stage.addChild(this.mouseAnimatedSprite);
+
+    // defining the texture arrays to switch between them when we switch directions
+    for (let i = 1; i <= 4; i++) {
+      this.texturesRight.push(this.sheet.textures['mouseRight_' + i + '.png']);
+      this.texturesLeft.push(this.sheet.textures['mouseLeft_' + i + '.png']);
+      this.texturesUp.push(this.sheet.textures['mouseUp_' + i + '.png']);
+      this.texturesDown.push(this.sheet.textures['mouseDown_' + i + '.png']);
+    }
+    this.textureMouseWon.push(this.sheet.textures['mouseWin_1.png']);
+    this.textureMouseWon.push(this.sheet.textures['mouseWin_2.png']);
+  }
+
+  gameLoop() {
+    if (this.isMouseRunning) {
+      // check if mouse collided with a cat, trap or cheese
+      const currentGridFieldValue = this.gridArray[this.currentGridPosition.row][this.currentGridPosition.column];
+      if (currentGridFieldValue === 0 || currentGridFieldValue === undefined) {
+        this.reset();
+        this.alertService.error('Mouse can\'t walk out of his path.', {autoClose: true});
+        return;
+      } else if (this.mouseAnimatedSprite.x + this.mouseAnimatedSprite.width > this.cat1Sprite.x &&
+                 Math.abs(this.mouseAnimatedSprite.y - this.cat1Sprite.y) < 10) {
+        this.mouseGotEatenOrTrapped('The cat beat up the mouse.');
+        return;
+      } else if (this.mouseAnimatedSprite.x - this.mouseAnimatedSprite.width < this.trapSprite.x &&
+        Math.abs(this.mouseAnimatedSprite.y - this.trapSprite.y) < 10) {
+        this.mouseGotEatenOrTrapped('The mouse got trapped.');
+        return;
+      } else if (this.mouseAnimatedSprite.y + this.mouseAnimatedSprite.height > this.cat2Sprite.y &&
+        Math.abs(this.mouseAnimatedSprite.x - this.cat2Sprite.x) < 10) {
+        this.mouseGotEatenOrTrapped('The cat beat up the mouse.');
+        return;
+      } else if (this.mouseAnimatedSprite.x + this.mouseAnimatedSprite.width > this.cheeseSprite.x &&
+                 Math.abs(this.mouseAnimatedSprite.y - this.cheeseSprite.y) < 10) {
+        if (this.numberOfDirectionBlocks > 7) {
+          this.alertService.error('You got the cheese but you used ' + this.numberOfDirectionBlocks + ' instead of 7 blocks.');
+          this.reset();
+          return;
+        } else {
+          this.isMouseRunning = false;
+          this.mouseAnimatedSprite.textures = this.textureMouseWon;
+          this.mouseAnimatedSprite.play();
+          this.alertService.success('Mouse got the cheese!\n Let\'s get to the next part.');
+          setTimeout(() => this.router.navigate(['/']), 1800);
+        }
+      }
+
+      // if current block is crossroad, check next move. If no next moves, continue the same direction
+      if (currentGridFieldValue === 2 &&
+        (this.lastCurve.row !== this.currentGridPosition.row ||
+          this.lastCurve.column !== this.currentGridPosition.column)) {
+        this.lastCurve.row = this.currentGridPosition.row;
+        this.lastCurve.column = this.currentGridPosition.column;
+        // don't switch to next block if it is undefined
+        if (this.movementDirectionBlocks[this.currentBlock + 1] !== undefined) {
+          this.currentBlock++;
+        }
+      }
+
+      // else move on
+      const directionBlock = this.movementDirectionBlocks[this.currentBlock];
+      if (directionBlock === undefined) {
+        this.reset();
+        this.alertService.error('You didn\'t make it.', {autoClose: true});
+        return;
+      } else if (directionBlock.type === 'moveRight') {
+        if (this.currentMove !== 'moveRight') {
+          this.currentMove = 'moveRight';
+          this.mouseAnimatedSprite.textures = this.texturesRight;
+          this.mouseAnimatedSprite.play();
+        }
+        this.mouseAnimatedSprite.x += 2;
+      } else if (directionBlock.type === 'moveLeft') {
+        if (this.currentMove !== 'moveLeft') {
+          this.currentMove = 'moveLeft';
+          this.mouseAnimatedSprite.textures = this.texturesLeft;
+          this.mouseAnimatedSprite.play();
+        }
+        this.mouseAnimatedSprite.x -= 2;
+      } else if (directionBlock.type === 'moveUp') {
+        if (this.currentMove !== 'moveUp') {
+          this.currentMove = 'moveUp';
+          this.mouseAnimatedSprite.textures = this.texturesUp;
+          this.mouseAnimatedSprite.play();
+        }
+        this.mouseAnimatedSprite.y -= 2;
+      } else {
+        if (this.currentMove !== 'moveDown') {
+          this.currentMove = 'moveDown';
+          this.mouseAnimatedSprite.textures = this.texturesDown;
+          this.mouseAnimatedSprite.play();
+        }
+        this.mouseAnimatedSprite.y += 2;
+      }
+
+      // set current grid position
+      if (this.mouseAnimatedSprite.x > this.paddingLeftRight + (this.currentGridPosition.column + 1.5) * this.cellWidth) {
+        this.currentGridPosition.column++;
+      } else if (this.mouseAnimatedSprite.x < this.paddingLeftRight + (this.currentGridPosition.column - 0.5) * this.cellWidth) {
+        this.currentGridPosition.column--;
+      } else if (this.mouseAnimatedSprite.y > this.paddingTopBottom + (this.currentGridPosition.row + 1.5) * this.cellHeight) {
+        this.currentGridPosition.row++;
+      } else if (this.mouseAnimatedSprite.y < this.paddingTopBottom + (this.currentGridPosition.row - 0.5) * this.cellHeight) {
+        this.currentGridPosition.row--;
+      }
+    }
   }
 
   runMouse() {
@@ -139,13 +335,22 @@ export class LessonFunctionsAssignmentComponent implements OnInit, OnDestroy {
       return;
     }
 
+    // collect the mouse directions
     if (topBlocks[0].type === 'myFunctionBody') {
       this.collectMouseDirectionsFromBlocks(topBlocks[0], topBlocks[1]);
     } else {
       this.collectMouseDirectionsFromBlocks(topBlocks[1], topBlocks[0]);
     }
 
-    this.buttonsDisabled = false;
+    // if there are no moves
+    if (this.movementDirectionBlocks.length === 0) {
+      this.buttonsDisabled = false;
+      this.alertService.warn('There are NO moves.', {autoClose: true});
+      return;
+    }
+
+    this.isMouseRunning = true;
+    this.mouseAnimatedSprite.play();
   }
 
   collectMouseDirectionsFromBlocks(functionDef: Blockly.Block, mouseDirectionBlocks: Blockly.Block) {
@@ -158,7 +363,6 @@ export class LessonFunctionsAssignmentComponent implements OnInit, OnDestroy {
     }
 
     const movementBlocks = mouseDirectionBlocks.getDescendants(true);
-    console.log(movementBlocks);
     this.numberOfDirectionBlocks = movementBlocks.length;
     movementBlocks.forEach((block) => {
       if (block.type === 'myFunction') {
@@ -167,6 +371,38 @@ export class LessonFunctionsAssignmentComponent implements OnInit, OnDestroy {
         this.movementDirectionBlocks.push(block);
       }
     });
+  }
+
+  mouseGotEatenOrTrapped(message: string) {
+    this.isMouseRunning = false;
+    this.mouseAnimatedSprite.visible = false;
+    this.mouseSpriteDead.position.set(this.mouseAnimatedSprite.x, this.mouseAnimatedSprite.y);
+    this.mouseSpriteDead.visible = true;
+    setTimeout(() => {
+      this.reset();
+    }, 3000);
+    this.alertService.error(message, {autoClose: true});
+  }
+
+  reset() {
+    this.isMouseRunning = false;
+    this.mouseAnimatedSprite.stop();
+    this.buttonsDisabled = false;
+    this.currentBlock = 0;
+    this.currentGridPosition = {
+      row: 1,
+      column: 0
+    };
+    this.lastCurve = {
+      row: undefined,
+      column: undefined
+    };
+    this.movementDirectionBlocks = [];
+    this.numberOfDirectionBlocks = 0;
+    this.mouseAnimatedSprite.textures = this.texturesRight;
+    this.mouseSpriteDead.visible = false;
+    this.mouseAnimatedSprite.visible = true;
+    this.mouseAnimatedSprite.position.set(this.paddingLeftRight + 0.5 * this.cellWidth, this.paddingTopBottom + this.cellHeight * 1.5);
   }
 
   onCode(code: string) {
